9454b348f1e52aa3b64d6c4a5eb87e0a9f89ec61
Rik Schennink <rikschennink@gmail.com>
2017-07-17 17:55:17 +0200

    improve uninstall comments

--- snippy/uninstall.php
+++ snippy/uninstall.php
@@ -5,24 +5,20 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
 
+// need these files do clean up
 require_once('includes/db.php');
 require_once('includes/utils.php');
 
-// delete resources
+// delete resources found in bits
 $bits = Data::get_entries_all('bits');
-
 foreach ( $bits as $bit ) {
-
     if (Utils::get_bit_type($bit) !== 'resource') {
         continue;
     }
-
-    $path = wp_upload_dir()['basedir'];
+    $path = \wp_upload_dir()['basedir'];
     $file = $path . $bit['value'];
-
-    // delete file
     @unlink($file);
 }
 
-// clear database
+// drop all created tables
 Data::drop_db();
